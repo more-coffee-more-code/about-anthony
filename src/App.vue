6276fd49f5f3416865fce2f9670e1146b31c8e4b
aruffincs@gmail.com <aruffincs@gmail.com>
2019-04-12 10:29:30 -0400

    changed font color

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #fafafa;
 }
 #nav {
   padding: 30px;
