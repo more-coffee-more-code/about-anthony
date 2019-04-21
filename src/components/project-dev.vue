d3f1f8500d8de36011bea4aa11726d5828678e4b
Anthony Ruffin <aruffincs@gmail.com>
2019-04-20 20:02:11 -0700

    updates

---
+++ src/components/project-dev.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="project-dev">
+    <h1 class="project-dev__project-name">title</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'project-dev',
+  props: [
+    
+  ]
+}
+</script>
+<style lang="scss">
+.project-dev {
+  &__project-name {
+    font-size: 1.4rem;
+  }
+}
+</style>
--- src/components/section-experience-dev.vue
+++ src/components/section-experience-dev.vue
@@ -2,12 +2,21 @@
   <section class="section-experience-dev">
     <div class="section-experience-dev__content">
       <h1 class="section-experience-dev__header">Developer xp</h1>
+      <section class="section-experience-dev__experience-display">
+        <dev-project/>
+      </section>
     </div>
   </section>
 </template>
 <script>
+// import DevProject from 'project-dev.vue';
+import DevProject from '../components/project-dev.vue';
+
 export default {
-  name: 'section-experience-dev'
+  name: 'section-experience-dev',
+  components: {
+    DevProject
+  }
 }
 </script>
 <style lang="scss">
