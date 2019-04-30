bb025cde945f00664e37469e2f0533a0bc7d56d7
Anthony Ruffin <aruffincs@gmail.com>
2019-04-30 14:14:30 -0400

    added button component

---
+++ src/components/button-outlined.vue
@@ -0,0 +1,15 @@
+<template>
+  <button class="button-outlined">
+    Go to project > 
+  </button>
+</template>
+<script>
+export default {
+  name: 'button-outlined'
+}
+</script>
+<style lang="scss">
+.button-outlined {
+
+}
+</style>
