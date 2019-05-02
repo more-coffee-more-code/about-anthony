be1b1fcaa3cb41ef1c69a941f39161dbb7ee0331
Anthony Ruffin <aruffincs@gmail.com>
2019-05-02 17:13:30 -0400

    adjusted loader

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <loader v-if="isLoading" class="loader"/>
-    <router-view v-if="!isLoading"/>
+    <loader v-if="isLoading" class="app__loader"/>
+    <router-view class="app__home" v-if="!isLoading"/>
   </div>
 </template>
 <script>
@@ -20,7 +20,7 @@ export default {
   mounted() {
     setTimeout(() => {
       this.isLoading = false;
-    }, 2000);
+    }, 200);
   }
 }
 </script>
@@ -43,9 +43,21 @@ export default {
   }
 }
 
-.loader {
-  background-color: black;
-  overflow-y:hidden;
-}
+.app {
+  &__loader {
+    background-color: black;
+    overflow-y:hidden;
+    transition: .5s ease-in;
+  }
 
+  &__home {
+    animation-name: fade-in;
+    animation-duration: 0.8s;
+
+    @keyframes fade-in {
+      from { opacity: 0; }
+      to { opacity: 1}
+    }
+  }
+}
 </style>
--- src/components/loader.vue
+++ src/components/loader.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="loader">
-    ....loading  
   </div>
 </template>
 <script>
